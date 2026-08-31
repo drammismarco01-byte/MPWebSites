3193369ba43312472c4a605a2408e69f3d7316d3
drammismarco01-byte <drammismarco01@gmail.com>
2026-08-31 12:48:12 +0200

    Add files via upload

---
+++ database.js
@@ -0,0 +1,17 @@
+// database.js - Usa i nomi completi in inglese per matchare la mappa
+
+const databaseEsterno = {
+    "United States of America": 850,
+    "China": 940,
+    "India": 620,
+    "Brazil": 430,
+    "Russia": 310,
+    "Italy": 510,
+    "Germany": 200,
+    "France": 150,
+    "Australia": 90,
+    "Canada": 45,
+    "South Africa": 120,
+    "Japan": 780
+    // Puoi aggiungere altri paesi scrivendo il loro nome in inglese (es. "Spain": 300)
+};
